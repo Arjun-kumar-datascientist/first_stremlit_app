643b36ce2279467be97d7b9c3839c1ed674035fa
Arjun Kumar <121504280+Arjun-kumar-datascientist@users.noreply.github.com>
2022-12-28 18:04:50 +0530

    Update stremlit_appy.py

--- stremlit_appy.py
+++ stremlit_appy.py
@@ -3,6 +3,7 @@ import stremlit
 streamlit.title('my parents new healthy diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale,Spinach & Rocket WSmoothie')
-streamlit.text('hard-Boiled Free-Range Egg')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale,Spinach & Rocket WSmoothie')
+streamlit.text('🐔hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
