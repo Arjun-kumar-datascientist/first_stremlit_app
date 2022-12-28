23351ed0999c3a628e038b903034d249bfc62804
Arjun Kumar <121504280+Arjun-kumar-datascientist@users.noreply.github.com>
2022-12-28 18:05:36 +0530

    Update stremlit_appy.py

--- stremlit_appy.py
+++ stremlit_appy.py
@@ -7,3 +7,5 @@ streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale,Spinach & Rocket WSmoothie')
 streamlit.text('🐔hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
