4490eda3dce408f697b82d1bdc6ee92ff51b3e7f
Arjun Kumar <121504280+Arjun-kumar-datascientist@users.noreply.github.com>
2022-12-28 18:00:38 +0530

    Update stremlit_appy.py

--- stremlit_appy.py
+++ stremlit_appy.py
@@ -1,3 +1,8 @@
 
 import stremlit
-stremlit.title('my parents new healthy diner')
+streamlit.title('my parents new healthy diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale,Spinach & Rocket WSmoothie')
+streamlit.text('hard-Boiled Free-Range Egg')
